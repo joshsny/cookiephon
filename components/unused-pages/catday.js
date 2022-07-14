3b820dc7dc3290a0aa31c94394ea6eb7b2ff602f
Maria Adamczyk <maryadamczyk4@gmail.com>
2022-07-14 11:33:14 +0200

    fix lint errors

---
+++ components/unused-pages/catday.js
@@ -0,0 +1,25 @@
+{
+  /* <section className="text-gray-600 bg-gradient-to-t from-fuchsia-600 to-white body-font">
+  <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+    <FaCat className="text-indigo-600 w-56 h-56"></FaCat>
+    <div className="text-center lg:w-2/3 w-full">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        Co powiesz na dzień kota?
+      </h1>
+      <p className="mb-8 leading-relaxed text-indigo-50">
+        Zastanawiasz się co podarować na święta siostrze, bratu lub chociaż by
+        kumplowi? Święta co raz bliżej a ty nadal nie masz prezentu? Tak się
+        składa, że idealnie trafiłeś. Cookiephon M23 z świąteczną okładką to
+        idealny prezent dla każdego.
+      </p>
+      <div className="flex justify-center">
+        <button className="ml-4 inline-flex text-black bg-indigo-600 border-0 py-2 px-6 focus:outline-none hover:bg-fuchsia-400 rounded text-lg">
+          Dowiedz się więcej
+        </button>
+      </div>
+    </div>
+  </div>
+</section>; */
+}
+
+const CatDayPage = () => <></>;
---
+++ components/unused-pages/cookiephonholiday.js
@@ -0,0 +1,25 @@
+{
+  /* <section className="text-gray-600 bg-gradient-to-t from-yellow-400 to-white body-font">
+  <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+    <FaCookieBite className="text-amber-700 w-56 h-56"></FaCookieBite>
+    <div className="text-center lg:w-2/3 w-full">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        Cookiephony obchodzą 1 urodziny 🥳🎉!
+      </h1>
+      <p className="mb-8 leading-relaxed text-black">
+        Zastanawiasz się co podarować na święta siostrze, bratu lub chociaż by
+        kumplowi? Święta co raz bliżej a ty nadal nie masz prezentu? Tak się
+        składa, że idealnie trafiłeś. Cookiephon M23 z świąteczną okładką to
+        idealny prezent dla każdego.
+      </p>
+      <div className="flex justify-center">
+        <button className="ml-4 inline-flex text-black bg-amber-700 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-400 rounded text-lg">
+          Dowiedz się więcej
+        </button>
+      </div>
+    </div>
+  </div>
+</section>; */
+}
+
+export const HolidayPage = () => <></>;
